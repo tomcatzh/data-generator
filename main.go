93b127077264439fcc302add97b4a41fca514a56
Zhang, Xiaofeng <i@zxf.io>
2017-06-07 00:06:08 +0800

    Ticket throttle goroutine

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"runtime"
 	"sync"
 	"time"
 
 	"github.com/tomcatzh/data-generator/data"
+	"github.com/tomcatzh/data-generator/ticket"
 )
 
 func main() {
@@ -22,16 +24,24 @@ func main() {
 	template, err := data.NewTemplate(templateFile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when read template: %v", err)
+		os.Exit(2)
 	}
 
 	var wg sync.WaitGroup
 
+	ticket, err := ticket.NewGoTicket(runtime.GOMAXPROCS(0))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when read template: %v", err)
+		os.Exit(2)
+	}
+
 	for f := range template.Iterate() {
 		wg.Add(1)
 
+		ticket.Take()
 		go func(f data.FileData) {
+			defer ticket.Return()
 			defer wg.Done()
-
 			f.Save()
 		}(f)
 	}
